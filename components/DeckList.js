91e15b2dffe4d2ec0c2f0299b21c602db0b473fc
Ayush Majumder <majumdershuvu@gmail.com>
2020-06-20 13:24:20 +0530

    Created the homepage which contains a tab view which lets user navigate between Decks and Add Decks page

---
+++ components/DeckList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View } from 'native-base';
+
+class DeckList extends Component {
+    render() {
+        return (
+            
+        );
+    }
+}
+
+export default DeckList;
