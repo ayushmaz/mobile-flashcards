6815018fd8b723c726d85dc373905b3c6d1b754f
Ayush Majumder <majumdershuvu@gmail.com>
2020-06-21 00:24:06 +0530

    commit

--- App.js
+++ App.js
@@ -1,19 +1,89 @@
+// import * as React from 'react';
+// import { Platform, StyleSheet, Text, View } from 'react-native';
+// import DecksPage from './components/DecksPage';
+// import AddCard from './components/AddCard';
+// import AddDeck from './components/AddDeck';
+// import HomePage from './components/HomePage';
+import 'react-native-gesture-handler';
 import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import DecksPage from './components/DecksPage';
-import AddCard from './components/AddCard';
-import AddDeck from './components/AddDeck';
-import HomePage from './components/HomePage';
+import { Button, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Go to Profile"
+        onPress={() => navigation.navigate('Profile')}
+      />
+    </View>
+  );
+}
+
+function ProfileScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Go to Notifications"
+        onPress={() => navigation.navigate('Notifications')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Go to Settings"
+        onPress={() => navigation.navigate('Settings')}
+      />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+function SettingsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Notifications" component={NotificationsScreen} />
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
+
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
   android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
 });
 
-export default function App() {
-  return (
-    <View style = {{flex : 1}}> 
-        <HomePage />
-    </View>
-  );
-}
+// export default function App() {
+//   return (
+//     <View style = {{flex : 1}}> 
+//         <HomePage />
+//     </View>
+//   );
+// }
